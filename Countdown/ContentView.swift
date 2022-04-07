fe5e31888f47bf6cf8bdd51797857a48ff8839db
Max Stephenson <max@Maxs-MacBook-Pro.local>
2022-04-06 21:40:45 -0500

    Start of the new project with and OLD folder for the previous for the time being

---
+++ Countdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by Max Stephenson on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Max Stephenson on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
